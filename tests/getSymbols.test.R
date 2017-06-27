d80cef8a262e5397c6a126bb472ceea28cd2fd76
i <isomorphisms@sdf.org>
2017-06-26 22:31:07 -0400

    :::
    `checkException` or `checkEquals` not exported from `RUnit`?

--- tests/getSymbols.test.R
+++ tests/getSymbols.test.R
@@ -1,4 +1,5 @@
 require(RUnit)
+require(quantmod)
 
 test.getSymbols.FRED.NFP <- function() {
 	purpose <- "Testing (at least): net connectivity; \n\t getSymbols.FRED thru its src='FRED' (all caps!) method; \n\t and that the first date in the source is what it was when I wrote this test!"
@@ -9,7 +10,7 @@ test.getSymbols.FRED.NFP <- function() {
 	# should be saved
 	first.day.of.nfp.new.dynamic <- head(PAYEMS,1)
 
-	RUnit::checkEquals( first.day.of.nfp.mine.static, first.day.of.nfp.new.dynamic, msg=purpose )
+	RUnit:::checkEquals( first.day.of.nfp.mine.static, first.day.of.nfp.new.dynamic, msg=purpose )
 }
 
 
@@ -17,8 +18,8 @@ test.getSymbols.oanda.xaucad <- function() {
 	purpose <- "Testing (at least): net connectivity; \n\t currency specification \n\t new.env() call \n\t auto.assign=TRUE \n\t getSymbols.FRED thru its src='FRED' (all caps!) method; \n\t and that the date restriction (last 180 days) is what it was when I wrote this test!"
 
 	currencies <- new.env()
-	RUnit::checkException(getSymbols.oanda('xaucad', env=currency, auto.assign=TRUE, from='2000-01-01'), msg="wrong currency specification")
-	RUnit::checkException(getSymbols.oanda('XAU/CAD', env=currency, from='2000-01-01'), msg="no auto.assign warning")
-	RUnit::checkException(getSymbols.oanda('XAU/CAD', auto.assign=TRUE, from='2000-01-01'), msg="need a new.env() to store it in!")
-	RUnit::checkException(getSymbols.oanda('XAU/CAD', env=currency, auto.assign=TRUE, from='2000-01-01'), msg="going back too far in dates")
+	RUnit:::checkException(getSymbols.oanda('xaucad', env=currency, auto.assign=TRUE, from='2000-01-01'), msg="wrong currency specification")
+	RUnit:::checkException(getSymbols.oanda('XAU/CAD', env=currency, from='2000-01-01'), msg="no auto.assign warning")
+	RUnit:::checkException(getSymbols.oanda('XAU/CAD', auto.assign=TRUE, from='2000-01-01'), msg="need a new.env() to store it in!")
+	RUnit:::checkException(getSymbols.oanda('XAU/CAD', env=currency, auto.assign=TRUE, from='2000-01-01'), msg="going back too far in dates")
 }
